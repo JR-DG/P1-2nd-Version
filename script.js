d48800f4b144a7aa42456d969f086fab218f43f7
Mario Alabi <junioralabi1@gmail.com>
2019-11-01 08:56:02 -0700

    1st p second version

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('select');
+//     var instances = M.FormSelect.init(elems, options);
+//   });
+
+$(document).ready(function(){
+    $('select').formSelect();
+  });
+
+    M.AutoInit();
